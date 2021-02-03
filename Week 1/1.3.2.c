4b3ad7096928e3f967179472d7e995c25446ceec
Timo van Sark <th.vansark@student.avans.nl>
2021-02-03 11:08:00 +0100

    opdrachten 1.3.2 en 1.3.3

--- Week 1/1.3.2.c
+++ Week 1/1.3.2.c
@@ -1,31 +1,27 @@
 #define F_CPU 8E6
 
 #include <avr/io.h>
-#include <util/delay.h>
 
 void wait( int ms )
 {
 	for (int i=0; i<ms; i++)
 	{
 		_delay_ms( 1 );
-	}
+	}	
 }
 
 int main( void )
 {
-	DDRD = 0xFF;			
-	DDRC = 0x00;
+	DDRD = 0xFF;
 	
 	while (1)
 	{
-		if (PINC & 0x01) 
-		{
-			PORTD = 0x80;
-			wait( 500 );
-			PORTD = 0x00;
-			wait( 500 );
-		} 		
+		PORTD = 0b01000000;
+		wait( 500 );
+		PORTD = 0b10000000;
+		wait( 500 );
 	}
-
+	
 	return 1;
 }
+
