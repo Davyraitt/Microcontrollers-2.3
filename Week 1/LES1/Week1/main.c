6a63ccc44945c6bb2ef54142159617f40e41268d
Davy <dj.raitt@student.avans.nl>
2021-02-03 09:56:50 +0100

    [Template Project Week 1]

---
+++ Week 1/LES1/Week1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Week1.c
+ *
+ * Created: 3-2-2021 09:52:47
+ * Author : Davy
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
